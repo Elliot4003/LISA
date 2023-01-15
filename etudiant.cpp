e72d59b39f14e37cc0ed2600d11f762221179bc3
sara-caballero <saracaballerofr@hotmail.com>
2023-01-15 12:30:20 +0100

    initial commit

---
+++ etudiant.cpp
@@ -0,0 +1,5 @@
+#include "etudiant.h"
+
+Etudiant::Etudiant(QString nom, QString prenom, int code, QString promotion):Personne(nom, prenom, code){     //constructeur
+    this->promotion = promotion;
+}
